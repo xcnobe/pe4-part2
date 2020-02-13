703979124c3d75259176b866eab442a431523eff
Noah Jonathan Berry <noahjberry@cu-tcom-2-10.203.11.163.int.colorado.edu>
2020-02-13 13:19:40 -0700

    PlaceMaker added

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -24,6 +24,27 @@ void DisplayBoard(std::vector<std::vector<char>> vec){
 }
 
 
+std::vector<std::vector<char>> PlaceMarker(std::vector<std::vector<char> > vec, string location, char marker){
+
+    if(location == "TL"){vec[0][0] = marker;}
+
+    if(location == "TM"){vec[0][1] = marker;}
+
+    if(location == "TR"){vec[0][2] = marker;}
+
+    if(location == "CL"){vec[1][0] = marker;}
+
+    if(location == "CM"){vec[1][1] = marker;}
+
+    if(location == "CR"){vec[1][2] = marker;}
+
+    if(location == "BL"){vec[2][0] = marker;}
+
+    if(location == "BM"){vec[2][1] = marker;}
+
+    if(location == "BR"){vec[2][2] = marker;}
+
+    return vec;
 
 int main(){
     std::vector<std::vector<char>> vec = CreateBoard();
